3d9af9e65e46a35550ab54570963b2d97fd6517b
SaefyahShemali <safonorthly@gmail.com>
2024-01-30 06:40:11 +0000

    new design

---
+++ final_design/data.py
@@ -0,0 +1,262 @@
+import os
+import json
+import csv
+import re
+import pandas as pd
+import prompt_bank
+
+def read_data(filename: str):
+    data = []
+    with open(filename, mode ='r')as file:
+        csvFile = csv.reader(file)  
+        for lines in csvFile:
+            data.append(lines)
+    return data
+
+def read_data_pado(filename: str):
+    data = []
+    with open(filename, mode ='r')as file:
+        txtFile = file.readlines()
+        txtFile = remove_items(txtFile,'\t\n')
+        for i in txtFile:
+            line = i.split("\t")
+            data.append(line)
+            
+        return data
+
+def read_data_mcrae(filename: str):
+    data = []
+    with open(filename, mode ='r')as file:
+        txtFile = file.readlines()
+        txtFile = remove_items(txtFile, '\n')
+        for i in txtFile:
+            line = i.split(" ")
+            data.append(line)
+            
+        return data
+
+def is_file_exits(filename: str):
+    return os.path.exists(filename)
+
+def open_result_file(filename: str, fields: []):
+    with open(filename, 'a') as csvfile: 
+        csvwriter = csv.writer(csvfile) 
+        csvwriter.writerow(fields)
+
+def record_exist(result_file_name: str, predicate: str, argument: str, roleType: str):
+    
+    # for ferretti Instrument and Location
+    if 'ferretti' in result_file_name:
+        with open(result_file_name, mode ='r')as file:
+            csvFile = csv.reader(file)
+            for line in csvFile:
+                if predicate in line[0] and argument in line[1]:
+                        return True
+        return False
+    
+    else:
+        # for Pado and Mcrae 
+        with open(result_file_name, mode ='r')as file:
+            csvFile = csv.reader(file)
+            for line in csvFile:
+                if predicate in line[0] and argument in line[1] and roleType in line[2]:
+                    return True
+        return False
+
+def record_exist_other(result_file_name: str, predicate: str, argument: str, roleType: str):
+    
+    # Get the Result
+    result = read_data(result_file_name)
+    result.pop(0)
+
+    for i in range(len(result)):
+        p = result[i][0]
+        a = result[i][1]
+        t = result[i][2]
+        
+        if p == predicate and a == argument and t == roleType:
+            return True
+        
+    return False
+
+def save_result(filename: str, result: []):
+    with open(filename, 'a') as csvfile: 
+        # creating a csv writer object  
+        csvwriter = csv.writer(csvfile) 
+    
+        # writing the data rows  
+        csvwriter.writerow(result) 
+
+# def save_reasons(filename: str, result: []):
+#     df = pd.DataFrame(result)
+#     df.to_csv("list_of_dict_to_csv.csv")
+
+def processing_json(responce: str, json_key: str ,filename_error: str, predicate: str, argument: str, roleType: str):
+    
+    try:
+        data = json.loads(responce)
+        return data[json_key]
+    
+    except json.JSONDecodeError as e:
+        print("Invalid JSON syntax:", e.msg)
+        save_result(filename_error, [predicate,argument,responce])
+
+        if 'Expecting property name enclosed in double quotes' in e.msg : 
+            dic = eval(responce)
+            dic_value = dic[json_key]
+            return dic_value
+        
+        if 'Expecting value' in e.msg:
+            print('just responcse: ', responce)
+            return responce
+        else:
+            exit()
+
+
+def get_back_exp_result(result_file_name: str, predicate: str, argument: str, argument_type: str):
+    
+    if not is_file_exits(result_file_name):
+        print('not exist file')
+        return
+    
+    # for ferretti Instrument and Location
+    if 'ferretti' in result_file_name:
+        with open(result_file_name, mode ='r')as file:
+            csvFile = csv.reader(file)
+            for line in csvFile:
+                if predicate in line and argument in line:
+                    return line
+        return None
+    
+    else:
+    # for Pado and Mrecrae 
+        with open(result_file_name, mode ='r')as file:
+            csvFile = csv.reader(file)
+            for line in csvFile:
+                if predicate in line and argument in line and argument_type in line:
+                    return line
+        return None
+
+def json_from_s(s):
+    match = re.findall(r"{.+[:,].+}|\[.+[,:].+\]", s)
+    return json.loads(match[0]) if match else None
+
+def remove_items(test_list, item): 
+    # remove the item for all its occurrences 
+    c = test_list.count(item) 
+    for i in range(c): 
+        test_list.remove(item) 
+    return test_list 
+
+def retrive_reasons(result_file_name: str,predicate: str, argument: str, roleType: str):
+     
+    reasons_conversation = []
+    json_key = 'Response'
+    reasons_prompts = prompt_bank.get_prompt('reasoning_lemma_tuple', predicate, argument, roleType, json_key)
+    
+    if not is_file_exits(result_file_name):
+        print('not exist file')
+        return None
+    
+    # for ferretti Instrument and Location
+    if 'ferretti' in result_file_name:
+        with open(result_file_name, mode ='r')as file:
+            csvFile = csv.reader(file)
+            for line in csvFile:
+                if predicate in line and argument in line:
+                    for i in range(3):
+                        reasons_conversation.append({"role": "user", "content": f"{reasons_prompts[i]}"})
+                        reasons_conversation.append({"role": "assistant", "content": f"{line[3+i]}"})
+
+            return reasons_conversation
+        return None
+    
+    else:
+    # for Pado and Mrecrae 
+        with open(result_file_name, mode ='r')as file:
+            csvFile = csv.reader(file)
+            for line in csvFile:
+                if predicate in line and argument in line and argument_type in line:
+                     for i in range(3):
+                        reasons_conversation.append({"role": "user", "content": f"{reasons_prompts[i]}"})
+                        reasons_conversation.append({"role": "assistant", "content": f"{line[3+i]}"})
+
+            print(reasons_conversation)                
+            return reasons_conversation
+        
+        return None
+
+# def check_result(result_file_name: str, predicate: str, argument: str, roleType: str):
+
+#     # for ferretti Instrument and Location
+#     if 'ferretti' in result_file_name:
+#         with open(result_file_name, mode ='r')as file:
+#             csvFile = csv.reader(file)
+#             for line in csvFile:
+#                 if not (predicate in line[0] and argument in line[1]):
+#                         return False
+#         return True
+
+#     else:
+#     # for Pado and Mcrae 
+#         with open(result_file_name, mode ='r')as file:
+#             csvFile = csv.reader(file)
+#             for line in csvFile:
+#                 if not (predicate in line[0] and argument in line[1] and roleType in line[2]):
+#                     return False
+#         return True
+
+def result_checking (dataset_file_name, result_file_name):
+    
+    if 'ferretti' in dataset_file_name:
+        # Get the data
+        dataset = read_data(dataset_file_name)
+       
+        for i in range (len(dataset)):
+            '''Data Extraction'''
+            predicate = dataset[i][0]
+            argument = dataset[i][1]
+
+            isExit = record_exist(result_file_name, predicate, argument, '')
+            
+            if not isExit:
+                print(f"{predicate} , {argument} does not exist at {i}")
+        
+        result = read_data(result_file_name)
+
+        if len(dataset)+1 != len(result):
+            return False
+            
+    else:
+        if 'pado' in dataset_file_name:
+            # Get the data
+            dataset = read_data_pado(dataset_file_name)
+
+        if 'mcrae' in dataset_file_name:
+            # Get the data
+            dataset = read_data_mcrae(dataset_file_name)
+            
+        for i in range (len(dataset)):
+            '''Data Extraction'''
+            predicate = dataset[i][0]
+            argument = dataset[i][1]
+            roleType = dataset[i][2]
+
+            isExit = record_exist(result_file_name, predicate, argument, roleType)
+            print(predicate,argument,roleType,isExit)
+            if not isExit:
+                print(f"{predicate} , {argument} , {roleType} does not exist at {i}")
+
+        result = read_data(result_file_name)
+
+        if len(dataset)+1 != len(result):
+            return False
+
+
+
+
+
+        
+
+    
+
---
+++ final_design/fit_scoring.py
@@ -0,0 +1,181 @@
+from models import Model
+import json
+import data
+import prompt_bank
+
+def textual_to_numerical_scale(level):
+
+    if "Near-Impossible" in level:
+        return 0.0
+    elif "Low" in level:
+        return 0.25
+    elif "Medium" in level:
+        return 0.5
+    elif "High" in level:
+        return 0.75
+    elif "Near-Perfect" in level:
+        return 1.0
+
+def simple_lemma_tuple(model: Model, predicate: str, argument: str, roleType: str, result_type: str):
+    
+    json_key = 'Fit score'
+    # The User Prompt
+    user_prompt = prompt_bank.get_prompt(f"simple_lemma_tuple_{result_type}", predicate, argument, roleType, json_key)
+    
+    '''TRACING'''
+    print('user_prompt : ', user_prompt)
+    
+    # Add the user prompt to the conversation 
+    model.conversation.append({"role": "user", "content": user_prompt})
+
+    # Interact with model
+    response_fit_score =  model.chat_with_model()       
+
+    '''TRACING'''
+    print('response_fit_score: ', response_fit_score)
+
+    # Processing JSON
+    fit_score = data.processing_json(response_fit_score,json_key, f"error_{json_key}.csv", predicate, argument, roleType)    
+
+    # Pop out the last user prompt to prevent bias in the evaluation
+    model.conversation.pop()
+
+    '''TRACING'''
+    print('conversation after:', model.conversation)
+    
+     # Convert it to a float number according to the resultType scale
+    if result_type == 'categorical':
+        return textual_to_numerical_scale(fit_score)
+    elif result_type == 'numerical':
+        return float(fit_score)
+
+def reasoning(model: Model, predicate: str, argument: str, roleType: str, reason_filename: str):
+
+    reasons_result  = [predicate, argument, roleType]
+    json_key = 'Response'
+    reasons=  prompt_bank.get_prompt('reasoning_lemma_tuple', predicate, argument, roleType, json_key)
+    print('reasons: \n', reasons)
+
+    for reason_prompt in reasons:
+        
+        '''TRACING'''
+        print('Before Adding Reason /n', model.conversation)
+
+        # Add the user prompt to the conversation 
+        model.conversation.append({"role": "user", "content": reason_prompt})
+
+        # Interact with model
+        response_reason = model.chat_with_model()
+        
+        '''TRACING'''
+        print('response_reason: \n', response_reason)
+        
+        # Processing JSON
+        reason = data.processing_json(response_reason,json_key, f"error_{json_key}.csv", predicate, argument, roleType)   
+
+        # Checking:
+        if not reason:
+            sys.exit("No Reason")   
+
+        # Store the reason for latter
+        reasons_result.append(reason)
+                
+        # Add it to the conversation
+        model.conversation.append({"role": "assistant", "content": f"{reason}"})
+
+        print('join reasons: ', reasons_result)
+        
+    # Saving the Reasons
+    data.save_result(reason_filename, reasons_result)
+
+
+def generate_sentence(model: Model, predicate: str, argument: str, roleType: str):
+    
+    json_key = 'Sentences'
+
+    # The User Prompt
+    gen_sentence_prompt = prompt_bank.get_prompt('gen_sentences', predicate, argument, roleType, json_key)
+   
+    '''TRACING'''
+    print('user_prompt : ', gen_sentence_prompt) 
+
+    # Add the user prompt to the conversation 
+    model.conversation.append({"role": "user", "content": gen_sentence_prompt})
+
+    # Interact with model
+    response_sentences =  model.chat_with_model()       
+
+    '''TRACING'''
+    print('response_sentences: ', response_sentences)
+
+    # Processing JSON
+    sentences = data.processing_json(response_sentences, json_key, f"error_{json_key}.csv", predicate, argument, roleType)    
+
+    # Pop out the last user prompt to prevent bias in the evaluation
+    model.conversation.pop()
+    
+    '''TRACING'''
+    print('sentences: ', sentences)
+
+    return sentences 
+
+def semantic_coherent(model: Model, predicate: str, argument: str, roleType: str, sentence: str):
+    
+    json_key = 'Is Fit'
+
+    # The User Prompt
+    check_semantic_prompt = prompt_bank.get_prompt('check_semantic', predicate, argument, roleType, json_key, sentence=sentence)
+   
+    '''TRACING'''
+    print('check_semantic_prompt : ', check_semantic_prompt) 
+    
+    # Add the user prompt to the conversation 
+    model.conversation.append({"role": "user", "content": check_semantic_prompt})
+    
+    # Interact with model
+    response_check_semantic = model.chat_with_model()
+    
+    '''TRACING'''
+    print('response_check_semantic: ', response_check_semantic)
+    
+    is_semantic = data.processing_json(response_check_semantic,json_key, f"error_{json_key}.csv", predicate, argument, roleType)
+    
+    model.conversation.append({"role": "assistant", "content": f"{is_semantic}"})
+
+    if 'No' in is_semantic:
+        return False
+    elif 'Yes' in is_semantic:
+        return True
+
+def simple_gen_sentences(model: Model, predicate: str, argument: str, roleType: str, result_type: str, sentence: str):
+
+    json_key = 'Fit score'
+    # The User Prompt
+    user_prompt = prompt_bank.get_prompt(f"simple_gen_sentences_{result_type}", predicate, argument, roleType, json_key, sentence=sentence)
+    
+    '''TRACING'''
+    print('user_prompt : ', user_prompt)
+    
+    # Add the user prompt to the conversation 
+    model.conversation.append({"role": "user", "content": user_prompt})
+
+    # Interact with model
+    response_fit_score =  model.chat_with_model()       
+
+    '''TRACING'''
+    print('response_fit_score: ', response_fit_score)
+
+    # Processing JSON
+    fit_score = data.processing_json(response_fit_score,json_key, f"error_{json_key}.csv", predicate, argument, roleType)    
+
+    # Pop out the last user prompt to prevent bias in the evaluation
+    model.conversation.pop()
+
+    '''TRACING'''
+    print('conversation after:', model.conversation)
+    
+     # Convert it to a float number according to the resultType scale
+    if result_type == 'categorical':
+        return textual_to_numerical_scale(fit_score)
+    elif result_type == 'numerical':
+        return float(fit_score)
